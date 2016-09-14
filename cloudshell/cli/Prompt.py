95d5be5aee14e25e3582ffeb49a74e3cf38d7c51
LuizaNacshon <luiza.n@quali.com>
2016-09-14 14:36:54 +0300

    changes added

--- cloudshell/cli/Prompt.py
+++ cloudshell/cli/Prompt.py
@@ -7,13 +7,12 @@ class Prompt():
         self.default_prompt = r'.*[>$#]\s*$'
         self.config_mode_promp = r'.*#\s*$'
         self.enable_prompt = '#\s*$'
+        self.current_prompt_regex = ''
 
     def default_prompt(self,prompt_regex):
         self.default_prompt = prompt_regex
 
 
-    def admin_prompt(self,prompt_regex):
-        self.enable_prompt = prompt_regex
+    def current_prompt(self,prompt_regex):
+        self.current_prompt_regex = prompt_regex
 
-    def config_mode_prompt(self,prompt_regex):
-        self.enable_prompt=prompt_regex
